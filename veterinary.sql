3311c2886ad47eff34c3c0b3ad94f55b8c3e96d6
unreisxz <reisxzoliveros@gmail.com>
2025-11-20 14:55:26 +0800

    feat/modify-lastname-dr-reyes-gonzales

--- veterinary.sql
+++ veterinary.sql
@@ -139,3 +139,6 @@ DELETE FROM appointments
 WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba')
   AND appointid = 8;
 
+UPDATE doctors
+SET dlastname = 'Reyes-Gonzales'
+WHERE dfirstname = 'Dr.Sofia';
